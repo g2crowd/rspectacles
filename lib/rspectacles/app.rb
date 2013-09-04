23015029bbd572a03c63f1053aed2f1b602e0584
Michael Wheeler <mwheeler@g2crowd.com>
2013-09-03 22:26:57 -0500

    Adding versioned asset helpers to update js and css

--- lib/rspectacles/app.rb
+++ lib/rspectacles/app.rb
@@ -10,15 +10,16 @@ module RSpectacles
   class App < Sinatra::Base
     connections = []
     dir = File.dirname(File.expand_path(__FILE__))
+    $public_folder = "#{dir}/app/public"
     set :app_file, __FILE__
     set :root, dir
     set :views, "#{dir}/app/views"
     set :static, true
 
     if respond_to? :public_folder
-      set :public_folder, "#{dir}/app/public"
+      set :public_folder, $public_folder
     else
-      set :public, "#{dir}/app/public"
+      set :public, $public_folder
     end
 
     uri = URI.parse 'redis://127.0.0.1:6379/'
@@ -52,6 +53,16 @@ module RSpectacles
 
       $emredis.pubsub.subscribe 'redis-rspec-examples' do |message|
         connections.each { |out| out << "data: #{message}\n\n" }
+      end
+    end
+
+    helpers do
+      def versioned_stylesheet(stylesheet)
+        url "/css/#{stylesheet}.css?" + File.mtime(File.join($public_folder, "css", "#{stylesheet}.css")).to_i.to_s
+      end
+
+      def versioned_javascript(js)
+        url "/js/#{js}.js?" + File.mtime(File.join($public_folder, "js", "#{js}.js")).to_i.to_s
       end
     end
   end
